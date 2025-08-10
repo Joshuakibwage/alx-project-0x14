564a7c993af829e4c3fc247f639a7966ac9065eb
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-08-10 19:10:08 +0300

    feat: add header and footer in the layout component

--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,16 +1,16 @@
-// import Header from "@/components/layouts/Header";
-// import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import Footer from "@/components/layouts/Footer";
 
 
-// const Layout: React.FC = ({ children }) => {
-//     return (
-//         <>
-//             <Header />
-//                 <div className="min-h-screen">{children}</div>
-//             <Footer />
-//         </>
-//     )
-// }
+const Layout: React.FC = ({ children }) => {
+    return (
+        <>
+            <Header />
+                <div className="min-h-screen">{children}</div>
+            <Footer />
+        </>
+    )
+}
 
 
-// export default Layout;
+export default Layout;
